df9ab83a9625263b7f6f36d41ea3507ca0b0bca4
Olga <ti-dzhy@yandex.ru>
2024-01-20 22:22:12 +0300

    добавили папку Task3 с решением третьей задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 3
+//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//40 => 4, 96 => 9, 72 => 7
+
+int a = 72;
+int maxdigit = 0;
+if ( 10 < a && a < 99 )
+{
+   int firstdigit = a / 10;
+   int seconddigit = a % 10;
+   if (firstdigit > seconddigit)
+   { maxdigit = firstdigit; }
+   else { maxdigit = seconddigit; }
+}
+Console.Write($"наибольшая цифра числа {maxdigit}");
+
+
+
